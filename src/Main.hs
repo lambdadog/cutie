37ed943a5e7a01ce3d620c0af683dc7ccd872004
Ashlynn Anderson <ashlynn.anderson@tutanota.com>
2019-05-08 19:45:51 -0400

    Setup basic network connection

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Network.Simple.TCP.TLS
+import qualified Data.ByteString as B
+import System.Environment
+
+-- Usage: cutie "example.irc.network" 6697
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do (address:port:_) <- getArgs -- gives an awful error, TODO: fix
+          -- Only one connection at once, so having the ServiceID suffix be empty is fine
+          clientSettings <- getDefaultClientSettings (address, B.empty) 
+          connect clientSettings address port
+            $ \(context, remoteAddr) ->
+                do putStrLn $ "Connection established to " ++ show remoteAddr
