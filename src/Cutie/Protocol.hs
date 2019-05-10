f88fa1e04b56a723f0a38409f2578e0d7dace286
Ashlynn Anderson <ashlynn.anderson@tutanota.com>
2019-05-10 01:13:39 -0400

    Refactor a bit, clarify the design overview.

--- src/Cutie/Protocol.hs
+++ src/Cutie/Protocol.hs
@@ -1,8 +1,11 @@
 module Cutie.Protocol where
 
+import qualified Data.ByteString as B
+
 -- Only support what we need to here, no need to get fancy
 
-data Message = Message [Tag] Prefix Command
+-- The Maybe bit here is arguably a bit unnecessary, but adds to clarity
+data Message = Message (Maybe [Tag]) Prefix Command
 
 -- TODO: Make the types for all of these more indicative of the actual
 -- meaning of the field -- a big one is a "Nick" type, or something
@@ -11,7 +14,9 @@ data Tag = Label String -- draft
          | Batch String
          | UnknownTag String String
 
-data Prefix = Prefix String String String
+-- Possibly should be cleaned up a bit, move the second bit out to
+-- "userdata" or something?
+data Prefix = Prefix String (Maybe String) (Maybe String)
 
 data Command = Nick String (Maybe Int)
              | User String String String String
@@ -31,3 +36,11 @@ data Capability = LabeledResponseCapability -- draft
                 | BatchCapability
                 | UnknownCapability String
 
+-- The magic function that turns a string into this mess above. It's
+-- fake for now instead of undefined just so the program stays
+-- functioning.
+decode :: B.ByteString -> Message
+decode _ = Message Nothing (Prefix "" Nothing Nothing) (UnknownCommand "")
+
+encode :: Message -> B.ByteString
+encode = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Network.Simple.TCP.TLS
 import qualified Data.ByteString as B
 import Data.ByteString.Char8 (pack, unpack)
 import System.Environment
+import qualified Cutie.Protocol as IRC
 
 -- Usage: cutie "example.irc.network" 6697 cutie_bot
 
@@ -17,9 +18,22 @@ main = do (address:port:nick:_) <- getArgs -- gives an awful error, TODO: fix
                    send context $ pack $ "NICK " ++ nick ++ "\r\n"
                    send context $ pack $ "USER " ++ nick ++" 0 * :Cutie bot!\r\n"
                    sockLoop context
-                     where
-                       sockLoop c =
-                         do Just dat <- recv c
-                            putStr $ unpack dat
-                            sockLoop c
-  
+
+-- I don't like the `IO ()` here, feels quite unclear, but we're doing
+-- socket IO. Might be nice to have some bit that would be unwrapped
+-- farther up to produce the IO? May be unnecessary though, and we're
+-- still logging to console for now, so not quite worth it at the
+-- moment honestly
+sockLoop :: Context -> IO ()
+sockLoop c = do Just dat <- recv c
+                putStr $ unpack dat -- log to console
+                case (handle $ IRC.decode dat) of
+                  Just message -> send c $ IRC.encode message
+                  Nothing -> return ()
+                sockLoop c
+
+-- Receive a message, and maybe respond, will be moved to another
+-- module later down the line.
+handle :: IRC.Message -> Maybe IRC.Message
+handle m = Nothing
+
