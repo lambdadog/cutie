97f1f2b7ec8a056f40c6bf84049c17bb814bf9bb
Ashlynn Anderson <ashlynn.anderson@tutanota.com>
2019-05-11 11:54:04 -0400

    Set haskell-hoogle-url no matter the mode
    For when I'm editing package.yaml, etc.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((haskell-mode
+((nil
   (haskell-hoogle-url . "https://www.stackage.org/lts-13.20/hoogle?q=%s")))
